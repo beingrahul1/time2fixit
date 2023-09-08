379bf0379ae07fc27135fd91640a11708d2d77c4
Ubuntu <ubuntu@ip-172-31-40-98.ap-south-1.compute.internal>
2023-09-08 06:24:54 +0000

    BoOking Service Commit

--- BookingService/src/main/java/com/stackroute/BookingService/BookingServiceApplication.java
+++ BookingService/src/main/java/com/stackroute/BookingService/BookingServiceApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookingServiceApplication {
-
+//Main Application
 	public static void main(String[] args) {
 		SpringApplication.run(BookingServiceApplication.class, args);
 	}
 
 }
+
