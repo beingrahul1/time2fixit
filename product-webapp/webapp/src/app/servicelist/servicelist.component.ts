bbd9973114274b1ef9563cc02a01c21c9fd71a01
Ubuntu <ubuntu@ip-172-31-34-40.ap-south-1.compute.internal>
2023-09-12 17:19:35 +0000

    service provider list UI

---
+++ product-webapp/webapp/src/app/servicelist/servicelist.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { serviceprovider } from '../service-provider-registration/serviceProvider.model';
+import { ServiceproviderService } from '../serviceprovider.service';
+
+@Component({
+  selector: 'app-servicelist',
+  templateUrl: './servicelist.component.html',
+  styleUrls: ['./servicelist.component.css']
+})
+export class ServicelistComponent{
+
+
+  
+}
