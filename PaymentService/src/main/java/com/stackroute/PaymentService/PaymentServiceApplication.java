04f0451e50a57e56b2f1f80645700a1f10ed99b7
Ubuntu <ubuntu@ip-172-31-40-98.ap-south-1.compute.internal>
2023-09-13 10:38:27 +0000

    Payment Service

--- PaymentService/src/main/java/com/stackroute/PaymentService/PaymentServiceApplication.java
+++ PaymentService/src/main/java/com/stackroute/PaymentService/PaymentServiceApplication.java
@@ -2,6 +2,9 @@ package com.stackroute.PaymentService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PaymentServiceApplication {
@@ -9,5 +12,15 @@ public class PaymentServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentServiceApplication.class, args);
 	}
+	@Bean
+    public WebMvcConfigurer corsConfigurer() 
+    {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+            }
+        };
+    }
 
 }
