610d9947f7066abd57379ce2318d9743695181f4
Ubuntu <ubuntu@ip-172-31-34-40.ap-south-1.compute.internal>
2023-09-13 07:21:53 +0000

    Service Provider list frontend updated

--- product-webapp/webapp/src/app/servicelist/servicelist.component.ts
+++ product-webapp/webapp/src/app/servicelist/servicelist.component.ts
@@ -7,7 +7,288 @@ import { ServiceproviderService } from '../serviceprovider.service';
   templateUrl: './servicelist.component.html',
   styleUrls: ['./servicelist.component.css']
 })
-export class ServicelistComponent{
+export class ServicelistComponent implements OnInit{
+
+  constructor(private serviceprovider:ServiceproviderService){}
+  ngOnInit(): void {
+    this.getServiceProviderList();
+  }
+
+  searchtxt : string ='';
+  shopName : boolean = false;
+  shopOwnerName : boolean = false;
+  email : boolean = false;
+  location : boolean = false;
+  phoneNumber : boolean = false;
+  productBrand : boolean = false;
+  serviceProduct : boolean = false;
+
+  listofproviders : serviceprovider [] =[];
+
+  onSubmit()
+  {
+    if(this.email)
+    {
+      this.serviceprovider.getByEmail().subscribe(
+        data=>{
+          this.listofproviders = data;
+        },
+        error=>{
+          console.log(error);
+        }
+      )
+    }
+
+    if(this.shopName && this.shopOwnerName)
+    {
+      this.serviceprovider.getByshopNameandShopOwnerName().subscribe(
+        data=>{
+          this.listofproviders = data;
+        },
+        error=>{
+          console.log(error);
+        }
+      )
+    }
+
+    if(this.shopName && this.serviceProduct)
+    {
+      this.serviceprovider.getByShopNameandServiceProduct().subscribe(
+        data=>{
+          this.listofproviders = data;
+        },
+        error=>{
+          console.log(error);
+        }
+      )
+    }
+
+    if(this.shopName && this.productBrand)
+    {
+      this.serviceprovider.getByShopNameandProductBrand().subscribe(
+        data=>{
+          this.listofproviders = data;
+        },
+        error=>{
+          console.log(error);
+        }
+      )
+    }
+
+    if(this.shopName && this.location)
+    {
+      this.serviceprovider.getByShopNameandAddress().subscribe(
+        data=>{
+          this.listofproviders = data;
+        },
+        error=>{
+          console.log(error);
+        }
+      )
+    }
+
+    if(this.shopName && this.phoneNumber)
+    {
+      this.serviceprovider.getByShopNameandContactNumber().subscribe(
+        data=>{
+          this.listofproviders = data;
+        },
+        error=>{
+          console.log(error);
+        }
+      )
+    }
+
+    if(this.shopName && this.email)
+    {
+      this.serviceprovider.getByShopNameandEmail().subscribe(
+        data=>{
+          this.listofproviders = data;
+        },
+        error=>{
+          console.log(error);
+        }
+      )
+    }
+
+    if(this.shopOwnerName && this.serviceProduct)
+    {
+      this.serviceprovider.getByShopOwnerNameandServiceProduct().subscribe(
+        data=>{
+          this.listofproviders = data;
+        },
+        error=>{
+          console.log(error);
+        }
+      )
+    }
+
+    if(this.shopOwnerName && this.productBrand)
+    {
+      this.serviceprovider.getByShopOwnerNameandProductBrand().subscribe(
+        data=>{
+          this.listofproviders = data;
+        },
+        error=>{
+          console.log(error);
+        }
+      )
+    }
+    if(this.shopOwnerName && this.phoneNumber)
+    {
+      this.serviceprovider.getByShopOwnerNameandcontactNumber().subscribe(
+        data=>{
+          this.listofproviders = data;
+        },
+        error=>{
+          console.log(error);
+        }
+      )
+    }
+
+    if(this.shopOwnerName && this.email)
+    {
+      this.serviceprovider.getByShopOwnerNameandEmail().subscribe(
+        data=>{
+          this.listofproviders = data;
+        },
+        error=>{
+          console.log(error);
+        }
+      )
+    }
+
+    if(this.serviceProduct && this.location)
+    {
+      this.serviceprovider.getByServiceProductandAddress().subscribe(
+        data=>{
+          this.listofproviders = data;
+        },
+        error=>{
+          console.log(error);
+        }
+      )
+    }
+
+    if(this.productBrand && this.location)
+    {
+      this.serviceprovider.getByProductBrandandAddress().subscribe(
+        data=>{
+          this.listofproviders = data;
+        },
+        error=>{
+          console.log(error);
+        }
+      )
+    }
+
+    if(this.serviceProduct && this.phoneNumber)
+    {
+      this.serviceprovider.getByServiceProductandcontactNumber().subscribe(
+        data=>{
+          this.listofproviders = data;
+        },
+        error=>{
+          console.log(error);
+        }
+      )
+    }
+    if(this.productBrand && this.phoneNumber)
+    {
+      this.serviceprovider.getByProductBrandandcontactNumber().subscribe(
+        data=>{
+          this.listofproviders = data;
+        },
+        error=>{
+          console.log(error);
+        }
+      )
+    }
+    if(this.serviceProduct && this.email)
+    {
+      this.serviceprovider.getByServiceProductandEmail().subscribe(
+        data=>{
+          this.listofproviders = data;
+        },
+        error=>{
+          console.log(error);
+        }
+      )
+    }
+
+    if(this.productBrand && this.email)
+    {
+      this.serviceprovider.getByProductBrandandEmail().subscribe(
+        data=>{
+          this.listofproviders = data;
+        },
+        error=>{
+          console.log(error);
+        }
+      )
+    }
+
+    if(this.serviceProduct && this.productBrand)
+    {
+      this.serviceprovider.getByServiceProductandProductBrand().subscribe(
+        data=>{
+          this.listofproviders = data;
+        },
+        error=>{
+          console.log(error);
+        }
+      )
+    }
+
+    if(this.location && this.phoneNumber)
+    {
+      this.serviceprovider.getByAddressandContactNumber().subscribe(
+        data=>{
+          this.listofproviders = data;
+        },
+        error=>{
+          console.log(error);
+        }
+      )
+    }
+
+    if(this.location && this.email)
+    {
+      this.serviceprovider.getByAddressandEmail().subscribe(
+        data=>{
+          this.listofproviders = data;
+        },
+        error=>{
+          console.log(error);
+        }
+      )
+    }
+
+    if(this.location && this.email && this.phoneNumber && this.shopName && this.shopOwnerName && this.productBrand && this.serviceProduct)
+    {
+      this.serviceprovider.getByAddressandEmail().subscribe(
+        data=>{
+          this.listofproviders = data;
+        },
+        error=>{
+          console.log(error);
+        }
+      )
+    }
+  }
+
+  getServiceProviderList()
+  {
+    this.serviceprovider.serviceProviderList().subscribe(
+      data=>{
+        this.listofproviders = data;
+      },
+      error=>{
+        console.log(error);
+      }
+    )
+  }
+
 
 
   
--- product-webapp/webapp/src/app/serviceprovider.service.ts
+++ product-webapp/webapp/src/app/serviceprovider.service.ts
@@ -11,4 +11,114 @@ export class ServiceproviderService {
   registerServiceProvider(sp: serviceprovider){
     return this.http.post<serviceprovider>(`http://localhost:1111/servicecenter/registration/na/signup`,sp)
   }
+
+  serviceProviderList()
+  {
+    return this.http.get<serviceprovider[]>('EndPoint');
+  }
+
+  getByEmail()
+  {
+    return this.http.get<serviceprovider[]>('EndPoint');
+  }
+
+  getByshopNameandShopOwnerName()
+  {
+    return this.http.get<serviceprovider[]>('EndPoint');
+  }
+
+  getByShopNameandServiceProduct()
+  {
+    return this.http.get<serviceprovider[]>('EndPoint');
+  }
+
+  getByShopNameandProductBrand()
+  {
+    return this.http.get<serviceprovider[]>('EndPoint');
+  }
+
+  getByShopNameandAddress()
+  {
+    return this.http.get<serviceprovider[]>('EndPoint');
+  }
+
+  getByShopNameandContactNumber()
+  {
+    return this.http.get<serviceprovider[]>('EndPoint');
+  }
+
+  getByShopNameandEmail()
+  {
+    return this.http.get<serviceprovider[]>('EndPoint');
+  }
+
+  getByShopOwnerNameandServiceProduct()
+  {
+    return this.http.get<serviceprovider[]>('EndPoint');
+  }
+
+  getByShopOwnerNameandProductBrand()
+  {
+    return this.http.get<serviceprovider[]>('EndPoint');
+  }
+
+  getByShopOwnerNameandcontactNumber()
+  {
+    return this.http.get<serviceprovider[]>('EndPoint');
+  }
+
+  getByShopOwnerNameandEmail()
+  {
+    return this.http.get<serviceprovider[]>('EndPoint');
+  }
+
+  getByProductBrandandAddress()
+  {
+    return this.http.get<serviceprovider[]>('EndPoint');
+  }
+
+  getByServiceProductandAddress()
+  {
+    return this.http.get<serviceprovider[]>('EndPoint');
+  }
+
+  getByServiceProductandcontactNumber()
+  {
+    return this.http.get<serviceprovider[]>('EndPoint');
+  }
+
+  getByProductBrandandcontactNumber()
+  {
+    return this.http.get<serviceprovider[]>('EndPoint');
+  }
+
+  getByProductBrandandEmail()
+  {
+    return this.http.get<serviceprovider[]>('EndPoint');
+  }
+
+  getByServiceProductandEmail()
+  {
+    return this.http.get<serviceprovider[]>('EndPoint');
+  }
+
+  getByServiceProductandProductBrand()
+  {
+    return this.http.get<serviceprovider[]>('EndPoint');
+  }
+
+  getByAddressandContactNumber()
+  {
+    return this.http.get<serviceprovider[]>('EndPoint');
+  }
+
+  getByAddressandEmail()
+  {
+    return this.http.get<serviceprovider[]>('EndPoint');
+  }
+
+  getByAllFields()
+  {
+    return this.http.get<serviceprovider[]>('EndPoint');
+  }
 }
